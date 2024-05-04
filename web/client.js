5a2c097b0610ec17e60ddadceca25f5e6caff6d5
Matthew Molloy <whamtet@gmail.com>
2024-05-05 06:24:53 +0900

    assoc question references

--- web/client.js
+++ web/client.js
@@ -1,6 +1,5 @@
 const $ = x => document.querySelector(x);
 const $$ = x => document.querySelectorAll(x);
-const $id = x => document.getElementById(x);
 
 htmx.config.defaultSwapStyle = 'outerHTML';
 
@@ -13,41 +12,62 @@ const hideModal = e => {
 const server = location.host.startsWith('localhost') ? 'http://localhost:3000' : 'https://app.simplifydd.com';
 
 // clicking pdf elements
-const onClick = e => {
+const onClickText = async e => {
   const span = e.target.parentElement;
   console.log('clicked', span);
+
+  const params = new URLSearchParams(location.search);
   const values = {
     fragment: span.innerText.trim(),
     fragmentId: span.id,
     page: PDFViewerApplication.page,
-    command: 'add'
+    offset: params.get('offset')
   };
 
-  htmx.ajax('POST', server + '/pdf-viewer/inset', {values, target: '#inset'});
+  const url = `${server}/api/question/${params.get('question_id')}/reference`;
+  const response = await fetch (url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(values)
+  });
+  if (response.status === 200) {
+    window.close();
+  }
 };
+
+const find = text => {
+  const {findBar} = PDFViewerApplication;
+  findBar.open();
+  $('#findInput').value = text;
+  findBar.dispatchEvent('again');
+}
 
 let virgin = true;
 const pageLoad = () => {
   if (virgin) {
     virgin = false;
     const params = new URLSearchParams(location.search);
-    if (params.has('page')) {
+    if (params.has('q')) {
+      find(params.get('q'));
+    } else if (params.has('page')) {
       PDFViewerApplication.page = Number(params.get('page'));
     }
-    htmx.trigger('#inset', 'click');
+    // htmx.trigger('#inset', 'click');
   }
   for (const span of $$('span[role=presentation]')) {
-    span.onclick = onClick;
+    span.onclick = onClickText;
   }
 }
 
 const updateAttribute = (el, k, f) => el.setAttribute(k, f(el.getAttribute(k)));
 
-// main
 updateAttribute($('#inset'), 'hx-get', x => {
   if (location.host.startsWith('localhost')) {
     return x;
   } else {
+    // index.html contains dev values
     return x.replace('http://localhost:3000', 'https://app.simplifydd.com');
   }
 });
